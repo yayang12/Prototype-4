40e4fa874babedb81d429fe2101f5bb84d3c9be0
yayang12 <yayang1@s.sfusd.edu>
2023-03-23 17:24:08 -0700

    Finished unit 4.4
    bug with unit4.4 last problem

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,5 +21,11 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave();
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(),powerupPrefab.transform.rotation);
     }
 
-    void SpawnEnemyWave()
-    { for (int i = 0; i < 3; i++)
-        {
-            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-        }
-    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType <Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+
+        void SpawnEnemyWave(int enemiesToSpawn)
+        {
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            }
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
