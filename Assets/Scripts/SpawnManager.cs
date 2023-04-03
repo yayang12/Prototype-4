d07a7451d75d8f2ad9fcd47deec9c94513861f70
yayang12 <yayang1@s.sfusd.edu>
2023-04-03 12:43:59 -0700

    Finished up to number 6 challenge

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,34 +9,36 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9.0f;
     public int enemyCount;
     public int waveNumber = 1;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnEnemyWave(waveNumber);
-        Instantiate(powerupPrefab, GenerateSpawnPosition(),powerupPrefab.transform.rotation);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
-    
+
     // Update is called once per frame
     void Update()
     {
-        enemyCount = FindObjectsOfType <Enemy>().Length;
+        enemyCount = FindObjectsOfType<Enemy>().Length;
 
-        if(enemyCount == 0)
+        if (enemyCount == 0)
         {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
             Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
 
-        void SpawnEnemyWave(int enemiesToSpawn)
+        
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
-            for (int i = 0; i < enemiesToSpawn; i++)
-            {
-                Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-            }
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
     }
 
